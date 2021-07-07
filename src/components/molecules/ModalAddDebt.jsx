815ca99d6ddc790e824db193b79899cf0a1b4986
Arthur Bruno <arthur.abbrr@gmail.com>
2021-07-07 15:56:11 -0300

    Modal com funcionalidade de adicionar dívida

---
+++ src/components/molecules/ModalAddDebt.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { Box, Button, makeStyles } from '@material-ui/core';
+import DefaultModal from '../atoms/DefaultModal';
+import DebtForm from '../atoms/DebtForm';
+import validation from '../../functions/utils/validation';
+import api from '../../functions/api';
+
+const useStyles = makeStyles({
+  form: {
+    margin: '25px 0',
+
+    '& form': {
+      display: 'grid',
+      gap: '20px',
+    },
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: '25px',
+  },
+});
+
+const ModalAddDebt = ({
+  open,
+  setOpen,
+  allUsers,
+  selectedUser,
+  getUsersAndDebts,
+}) => {
+  const classes = useStyles();
+  const [user, setUser] = useState(null);
+  const [reason, setReason] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [fieldsWithError, setFieldsWithError] = useState({});
+
+  const handleAddClick = async () => {
+    const validationResult = validation.isDebtValid({ user, reason, amount });
+
+    if (!validationResult.errors) {
+      setOpen(false);
+      await api.createDebt(user, reason, amount);
+      getUsersAndDebts();
+      setUser(null);
+      setReason('');
+      setAmount(0);
+    } else {
+      setFieldsWithError(validationResult.errors);
+    }
+  };
+
+  return (
+    <DefaultModal title="Adicionar dívida" open={open} setOpen={setOpen}>
+      <Box className={classes.form}>
+        <DebtForm
+          user={user}
+          setUser={setUser}
+          reason={reason}
+          setReason={setReason}
+          amount={amount}
+          setAmount={setAmount}
+          allUsers={allUsers}
+          parent="modal"
+          fieldsWithError={fieldsWithError}
+          setFieldsWithError={setFieldsWithError}
+        />
+      </Box>
+
+      <Box className={classes.button}>
+        <Button onClick={handleAddClick} variant="contained" color="primary">
+          Adicionar
+        </Button>
+      </Box>
+    </DefaultModal>
+  );
+};
+
+export default ModalAddDebt;
