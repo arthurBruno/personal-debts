7487deb9354173ecbfad871e5a44df6ab6ff2933
Arthur Bruno <arthur.abbrr@gmail.com>
2021-07-07 15:55:12 -0300

    Form para ser usado tanto na adição quando edição de dívidas

---
+++ src/components/atoms/DebtForm.jsx
@@ -0,0 +1,79 @@
+import { TextField, makeStyles } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CurrencyTextField from '../atoms/CurrencyTextField';
+
+const useStyles = makeStyles({
+  root: {
+    '& .MuiTextField-root': {
+      width: '100%',
+    },
+  },
+});
+
+const ModalAddDebt = ({
+  user,
+  setUser,
+  reason,
+  setReason,
+  amount,
+  setAmount,
+  allUsers,
+  parent,
+  fieldsWithError,
+  setFieldsWithError,
+}) => {
+  const classes = useStyles();
+
+  const handleChange = (setValue, value, name) => {
+    setValue(value);
+    console.log(setValue)
+    console.log(value)
+    console.log(name)
+    if (!!fieldsWithError[name]) {
+      const errors = fieldsWithError;
+      console.log(errors[name])
+      delete errors[name];
+      setFieldsWithError(errors);
+    }
+  };
+
+  return (
+    <form className={classes.root}>
+      <Autocomplete
+        options={allUsers}
+        getOptionLabel={(user) => user.name}
+        value={user}
+        onChange={(event, value) => handleChange(setUser, value, 'user')}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Usuário"
+            variant="outlined"
+            error={!!fieldsWithError.user}
+            helperText={fieldsWithError.user}
+          />
+        )}
+      />
+
+      <TextField
+        label="Descrição"
+        variant="outlined"
+        value={reason}
+        onChange={(e) => handleChange(setReason, e.target.value, 'reason')}
+        error={!!fieldsWithError.reason}
+        helperText={fieldsWithError.reason}
+      />
+
+      <CurrencyTextField
+        label="Valor"
+        id={parent}
+        value={amount}
+        onChange={(value) => handleChange(setAmount, value, 'amount')}
+        error={!!fieldsWithError.amount}
+        helperText={fieldsWithError.amount}
+      />
+    </form>
+  );
+};
+
+export default ModalAddDebt;
