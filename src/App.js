75ebfa8d5610db42290c359cf9ba44a1bf69eeed
Arthur Bruno <arthur.abbrr@gmail.com>
2021-07-07 15:54:27 -0300

    Big Bang

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ThemeProvider,
+  createTheme,
+} from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
+import Home from './components/pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const theme = createTheme({
+  overrides: {
+    MuiButton: {
+      contained: {
+        color: '#fff !important',
+        padding: '10px 25px !important',
+        backgroundColor: '#5C00A4 !important',
+        borderRadius: '4px !important',
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: '#5C00A4',
+    },
+    secondary: {
+      main: '#787878',
+    },
+    error: {
+      main: '#d62046',
+    },
+    warning: {
+      main: '#f17700',
+    },
+    info: {
+      main: '#44a0db',
+    },
+    success: {
+      main: '#48ae9f',
+    },
+    grey: {
+      main: '#c3c3c3'
+    }
+  },
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+    h1: {
+      fontSize: 96,
+    },
+    h2: {
+      fontSize: 60,
+      fontWeight: 300,
+    },
+    h3: {
+      fontSize: 48,
+      fontWeight: 300,
+    },
+    h4: {
+      fontSize: 34,
+    },
+    h5: {
+      fontSize: 24,
+    },
+    h6: {
+      fontSize: 18,
+    },
+    subtitle1: {
+      fontSize: 20,
+    },
+    subtitle2: {
+      fontSize: 14,
+    },
+    body1: {
+      fontSize: 16,
+    },
+    body2: {
+      fontSize: 14,
+    },
+    button: {
+      fontSize: 12,
+      letterSpacing: 0.6,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 12,
+    },
+    overline: {
+      fontSize: 10,
+      fontWeight: 300,
+    },
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Container maxWidth="lg">
+      <Home />
+    </Container>
+  </ThemeProvider>
+);
 
 export default App;
